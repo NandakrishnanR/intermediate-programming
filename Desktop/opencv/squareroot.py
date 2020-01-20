139a2b4ecea5c1f47e82b9fb6c2268c686ae0287
NANDU R <rnandakrishnan2001@gmail.com>
2020-01-20 21:26:34 +0530

    program to find the square root of the given number

---
+++ Desktop/opencv/squareroot.py
@@ -0,0 +1,5 @@
+import math
+n=int(input("enter the number to be found"))
+print(math.sqrt(n))
+#here math module is to be imported to this program first inorder to use square root function
+#math.sqrt(2)or any number and this is the syntax of square root program
